fdfa768ad0d4b4fa99745b09038140d719ac7cbd
Darren Good <darrengoodx@gmail.com>
2014-06-05 20:23:20 +0100

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,9 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title('Sample App')
     end
 
 
@@ -24,23 +24,38 @@ describe "Help page" do
       expect(page).to have_content('Help')
     end
 
-	it "should have the title 'Help'" do
+	it "should have the title 'Sample App | Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title('Sample App | Help')
     end
 
   end
 
 describe "About page" do
-
+  
     it "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
 	
-	it "should have the title 'About'" do
+	it "should have the title 'Sample App | About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title('Sample App | About')
+    end
+
+  end
+
+describe "Contact page" do
+    #
+  
+    it "should have the content 'Contact us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact us')
+    end
+  
+  it "should have the title 'Sample App | Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Sample App | Contact')
     end
 
   end
